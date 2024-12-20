2c83d55c6c0a238d96994d8a58d1585e76f47cc0
Xiao-Jinghan <915571873@qq.com>
2024-12-20 11:51:11 +0800

    insert

--- insert/insert.py
+++ insert/insert.py
@@ -1,5 +1,9 @@
 from insert_equipment_information import insert_equipment_information
 from insert_passive_description import insert_passive_description
+from insert_active_skill_description import insert_active_skill_description
+from insert_attribute_bonus_information import insert_attribute_bonus_information
 
 insert_equipment_information(host='localhost', user='root', password='jichenyu666.', database='damage_calculation')
 insert_passive_description(host='localhost', user='root', password='jichenyu666.', database='damage_calculation')
+insert_active_skill_description(host='localhost', user='root', password='jichenyu666.', database='damage_calculation')
+insert_attribute_bonus_information(host='localhost', user='root', password='jichenyu666.', database='damage_calculation')
